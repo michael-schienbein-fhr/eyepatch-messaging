ef44febf58058fea3c18921a5e3d1cba13131e86
Michael Schienbein <moomba0@gmail.com>
2021-10-11 22:04:18 +0700

    yes

---
+++ ouu.js
@@ -0,0 +1,6 @@
+// const server = require("./server");
+// const {PORT} = require("./config");
+
+// server.listen(PORT, () => {
+//   console.log(`Server started on port ${server.address().port} :)`);
+// });
