4712ba526d80858dc692b6f766b534bc475162b0
Michael Schienbein <moomba0@gmail.com>
2021-10-14 21:06:48 +0700

    remove redundant files

--- ouu.js
+++
@@ -1,6 +0,0 @@
-// const server = require("./server");
-// const {PORT} = require("./config");
-
-// server.listen(PORT, () => {
-//   console.log(`Server started on port ${server.address().port} :)`);
-// });
